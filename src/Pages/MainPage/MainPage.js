88c4591edceeae91434ddb4ac9f0478d6f5e06de
Seogeurim <geulims@naver.com>
2019-01-28 21:07:16 +0900

    React Router

---
+++ src/Pages/MainPage/MainPage.js
@@ -0,0 +1,23 @@
+// This Page is Skeleton of React Structure for Web Development
+// If you want to make other page, Copy and Refactor this page.
+
+import React, { Component } from "react";
+
+const defaultProps = {};
+const propTypes = {};
+
+class MainPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div>This is Main Page</div>;
+  }
+}
+
+MainPage.defaultProps = defaultProps;
+MainPage.propTypes = propTypes;
+
+export default MainPage;
---
+++ src/Pages/index.js
@@ -0,0 +1,5 @@
+/* Default Page */
+import DefaultPage from "./_DefaultPage/DefaultPage.react";
+import MainPage from "./MainPage/MainPage";
+
+export { DefaultPage, MainPage };
