2bc6e0108caa1438964381822bc3866b0c8e8ca2
Seogeurim <geulims@naver.com>
2019-03-12 00:19:40 +0900

    NavBar #1

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -36,35 +36,34 @@ class NavBar extends Component {
 
   render() {
     return (
-      <div>
-        <Navbar color="light" light expand="md">
-          <NavbarBrand href="/">reactstrap</NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="/components/">Components</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">
-                  GitHub
-                </NavLink>
-              </NavItem>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Options
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>Option 1</DropdownItem>
-                  <DropdownItem>Option 2</DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>Reset</DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-            </Nav>
-          </Collapse>
-        </Navbar>
-      </div>
+      <Navbar className="navBar" color="light" light expand="md">
+        <NavbarBrand href="/">LOVA</NavbarBrand>
+        <NavbarToggler onClick={this.toggle} />
+        <Collapse isOpen={this.state.isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="/link1/">Link1</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/link2/">Link2</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/link3/">Link3</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>Option 1</DropdownItem>
+                <DropdownItem>Option 2</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Reset</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+        </Collapse>
+      </Navbar>
     );
   }
 }
--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,5 +1,5 @@
 /* Default Page */
-import DefaultPage from "./_DefaultPage/DefaultPage.react";
+import DefaultPage from "./_DefaultPage/DefaultPage";
 import MainPage from "./MainPage/MainPage";
 
 export { DefaultPage, MainPage };
